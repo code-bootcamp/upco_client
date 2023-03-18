69a36d12f0ca1c7f232fcf6ff7695f99b4d42e7c
“eggmun98” <eggmun98@gmail.com>
2023-03-18 15:02:11 +0900

    [2023.03.18] Feat #17-지도마커여러개구현 영역좌표출력 리팩토링

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,4 +1,4 @@
-import MainUI from "../../src/components/units/main/main.index";
+import MainUI from "../../src/components/units/main/index";
 
 export default function MainPage(): JSX.Element {
   return <MainUI></MainUI>;
