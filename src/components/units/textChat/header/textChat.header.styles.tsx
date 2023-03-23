2cf00f739c1da439075fbeb60b2db5bcff263bd6
kkasol <kkasol92@gmail.com>
2023-03-23 16:27:21 +0900

    [2023.3.23] Feat 메시지 수신, 발신 구분

--- src/components/units/textChat/header/textChat.header.styles.tsx
+++ src/components/units/textChat/header/textChat.header.styles.tsx
@@ -1,13 +1,20 @@
 import styled from "@emotion/styled";
 
+interface MessageProps {
+  isSent: boolean;
+}
+
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   background-color: #efefef;
   border-right: 1px solid #d9d9d9;
+`;
 
-  > div {
-    color: #fff;
-    background-color: #999;
-  }
+export const Message = styled.div<MessageProps>`
+  max-width: 60%;
+  margin-bottom: 5px;
+  padding: 8px 16px;
+
+  word-wrap: break-word;
 `;
