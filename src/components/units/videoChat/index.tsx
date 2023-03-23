090d1f9ca250fd1b06a6a3492c39fbda752fa1ff
chlgusrb97 <ikolp34@gmail.com>
2023-03-23 17:49:12 +0900

    [2023.3.23] Feat #76-webRTC 마이카메라 화면 출력

--- src/components/units/videoChat/index.tsx
+++ src/components/units/videoChat/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { MediaRequest } from "../../commons/hooks/customs/mediaRequest";
 import * as S from "./styles";
 
 export default function VideoChat(): JSX.Element {
   const [isVolume, setIsVolume] = useState(false);
   const [isMic, setIsMic] = useState(false);
   const [isVideo, setIsVideo] = useState(false);
+
+  const { localVideoRef, remoteVideoRef } = MediaRequest();
 
   const onClickVolume = (): void => {
     setIsVolume((prev) => !prev);
@@ -21,13 +24,23 @@ export default function VideoChat(): JSX.Element {
   return (
     <>
       <S.Wrapper>
-        <S.OpponentVideo></S.OpponentVideo>
-        <S.Myvideo></S.Myvideo>
+        <S.OpponetVideoBox>
+          <video ref={localVideoRef} autoPlay />
+        </S.OpponetVideoBox>
+        {/* <S.MyvideoBox>
+          <video ref={localVideoRef} autoPlay />
+        </S.MyvideoBox> */}
         <S.IconBox>
           <li onClick={onClickVolume}>{isVolume ? <S.VolumeOn /> : <S.VolumeOff />}</li>
           <li onClick={onClickMic}>{isMic ? <S.MicOn /> : <S.MicOff />}</li>
           <li onClick={onClickVideo}>{isVideo ? <S.VideoOn /> : <S.VideoOff />}</li>
         </S.IconBox>
+        {/* <S.VideoBox>
+          <S.OpponetVideo ref={localVideoRef} autoPlay />
+          <S.MyvideoBox>
+            <video ref={remoteVideoRef} autoPlay muted />
+          </S.MyvideoBox>
+        </S.VideoBox> */}
       </S.Wrapper>
     </>
   );
