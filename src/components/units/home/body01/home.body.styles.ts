8d21f767ad73f3f1ae530214821120830e158e66
“eggmun98” <eggmun98@gmail.com>
2023-03-29 10:41:46 +0900

    [2023.03.29] Feat 페이지이동함수 구현

--- src/components/units/home/body01/home.body.styles.ts
+++ src/components/units/home/body01/home.body.styles.ts
@@ -4,5 +4,5 @@ export const Wrapper = styled.div`
   width: 100%;
   height: calc(100vh);
   /* background: linear-gradient(180deg, #9e4dab 0%, #6658ca 100%);  */
-  background-color: #6658ca;
+  /* background-color: #6658ca;  */
 `;
