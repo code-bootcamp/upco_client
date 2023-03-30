600d74e75ccbd9d8f5b79609e8c61ef1a878b14d
chlgusrb97 <ikolp34@gmail.com>
2023-03-30 15:16:52 +0900

    [2023.3.30] #143-문의내역페이지네이션해당페이지숫자색상Active기능

--- src/components/units/questionList/body/questionList.body.index.tsx
+++ src/components/units/questionList/body/questionList.body.index.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent, useState } from "react";
+import { getDate } from "../../../../commons/libraries/utils";
 import { FetchQuestionsSlice } from "../../../commons/hooks/customs/fetchQuestionsSlice";
 import QuestionListFooter from "../footer/questionList.footer.index";
 import * as S from "./questionList.body.styles";
@@ -7,45 +8,58 @@ export default function QuestionListBody(): JSX.Element {
   const fetchQuestions = FetchQuestionsSlice();
 
   const [page, setPage] = useState(0);
-  const [starPage, setStartPage] = useState(1);
+  const [starPage, setStartPage] = useState(0);
+  const [targetPage, setTargetPage] = useState(0);
   const lastPage = fetchQuestions?.length;
 
   const onClickPageNumber = (event: MouseEvent<HTMLSpanElement>): void => {
-    setPage(Number(event.currentTarget.id));
+    const targetPage = Number(event.currentTarget.id);
+    setPage(targetPage);
+    setTargetPage(targetPage);
   };
 
   const onClickLeftArrow = (): void => {
     setStartPage(starPage - 10);
     setPage(starPage - 10);
+    setTargetPage(starPage - 10);
   };
 
   const onClickRightArrow = (): void => {
     setStartPage(starPage + 10);
     setPage(starPage + 10);
+    setTargetPage(starPage + 10);
   };
-  console.log(page, starPage);
+
   return (
     <>
       <S.Wrapper>
         <S.Title>문의내역</S.Title>
-        {fetchQuestions?.[page]?.map((el, index) => (
-          <S.ContentsBox key={el.id}>
-            <S.TextBox>
-              <S.ContentsTitle>{el.title}</S.ContentsTitle>
-              <S.Contents>{el.contents}</S.Contents>
-            </S.TextBox>
-            <S.CreateAt>{el.createAt}</S.CreateAt>
-          </S.ContentsBox>
-        ))}
-        <QuestionListFooter
-          page={page}
-          starPage={starPage}
-          lastPage={lastPage}
-          setPage={setPage}
-          onClickPageNumber={onClickPageNumber}
-          onClickLeftArrow={onClickLeftArrow}
-          onClickRightArrow={onClickRightArrow}
-        />
+        {fetchQuestions && fetchQuestions?.length > 0 ? (
+          <>
+            <S.ContentsBox>
+              {fetchQuestions?.[page]?.map((el) => (
+                <S.ContentsList key={el.id}>
+                  <S.TextBox>
+                    <S.ContentsTitle>{el.title}</S.ContentsTitle>
+                    <S.Contents>{el.contents}</S.Contents>
+                  </S.TextBox>
+                  <S.CreateAt>{getDate(el.createAt)}</S.CreateAt>
+                </S.ContentsList>
+              ))}
+            </S.ContentsBox>
+            <QuestionListFooter
+              page={page}
+              starPage={starPage}
+              targetPage={targetPage}
+              lastPage={lastPage}
+              onClickPageNumber={onClickPageNumber}
+              onClickLeftArrow={onClickLeftArrow}
+              onClickRightArrow={onClickRightArrow}
+            />
+          </>
+        ) : (
+          <S.NoneBox>문의내역이 없습니다.</S.NoneBox>
+        )}
       </S.Wrapper>
     </>
   );
--- src/components/units/questionList/footer/questionList.footer.index.tsx
+++ src/components/units/questionList/footer/questionList.footer.index.tsx
@@ -2,18 +2,25 @@ import * as S from "./questionList.footer.styles";
 import { IQuestionListFooterProps } from "./questionList.footer.types";
 
 export default function QuestionListFooter(props: IQuestionListFooterProps): JSX.Element {
+  console.log(props.page === props.targetPage, "트루!");
+
   return (
     <S.Wrapper>
-      <button onClick={props.onClickLeftArrow} disabled={props.starPage === 1 ? true : false}>
+      <button onClick={props.onClickLeftArrow} disabled={props.starPage === 0 ? true : false}>
         <S.ArrowLeft />
       </button>
       {new Array(10).fill(1).map(
         (_, index) =>
           props.lastPage &&
           props.starPage + index + 1 <= props.lastPage && (
-            <span onClick={props.onClickPageNumber} id={String(index + props.starPage)} key={index}>
-              {index + props.starPage}
-            </span>
+            <S.Number
+              key={index}
+              id={String(index + props.starPage)}
+              isActive={props.starPage + index === props.targetPage}
+              onClick={props.onClickPageNumber}
+            >
+              {index + props.starPage + 1}
+            </S.Number>
           )
       )}
       <button
