cd1864d75db17f4d8080316513c807b62e828f0a
chlgusrb97 <ikolp34@gmail.com>
2023-03-30 15:55:25 +0900

    [2023.3.30] Rename 페이지네이션기능 컴포넌트 재사용성을 위한 분리 처리

--- src/components/commons/paginations/questionList/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import { IQuestion } from "../../../../commons/types/generated/types";
-
-export const PaginationsQuestionList = () => {
-  const [startPage, setStartPage] = useState(1);
-  const lastPage = Math.ceil(p);
-};
--- src/components/units/notice/footer/notice.footer.index.tsx
+++ src/components/units/notice/footer/notice.footer.index.tsx
@@ -1,5 +1,3 @@
-import Pagination from "../../../commons/paginations/Pagination.container";
-
 export default function NoticeFooter(): JSX.Element {
-  return <Pagination />;
+  return <>1</>;
 }
