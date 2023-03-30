e3b6672cfab0b4200a4066da4d785cdadf4110ab
kkasol <kkasol92@gmail.com>
2023-03-30 20:51:06 +0900

    [2023.3.30] Feat 채팅 로그 불러오기

--- src/components/units/textChat/header/textChat.header.styles.tsx
+++ src/components/units/textChat/header/textChat.header.styles.tsx
@@ -14,35 +14,21 @@ export const Wrapper = styled.div`
     align-items: center;
     font-size: 24px;
     color: #999;
-  }
-`;
 
-export const Body = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 20px;
-  white-space: pre-wrap;
+    > div.sentMessage {
+      align-self: flex-end;
+      background-color: #f8e71c;
+      color: #333;
+    }
 
-  > div {
-    max-width: 60%;
-    word-wrap: break-word;
-    overflow-wrap: break-word;
-    padding: 10px;
-    margin: 5px;
-    border-radius: 5px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
-  }
-
-  > div.systemMessage {
-    margin: 0 auto;
-    color: #333;
-    padding: 5px 10px;
-  }
-
-  > div.sentMessage {
-    align-self: flex-end;
-    background-color: #f8e71c;
-    color: #333;
+    > div {
+      max-width: 60%;
+      word-wrap: break-word;
+      overflow-wrap: break-word;
+      padding: 10px;
+      margin: 5px;
+      border-radius: 5px;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+    }
   }
 `;
