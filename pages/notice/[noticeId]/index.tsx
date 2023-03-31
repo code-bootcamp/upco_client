b9495bb17d40a5a7149a457d8547e1907e8f05ba
“eggmun98” <eggmun98@gmail.com>
2023-03-31 11:14:34 +0900

    [2023.03.31] Design 공지사항 상세보기

---
+++ pages/notice/[noticeId]/index.tsx
@@ -0,0 +1,3 @@
+export default function NoticeDetailPage(): JSX.Element {
+  return <></>;
+}
---
+++ src/components/units/noticeDetatil/header/notice.header.tsx
@@ -0,0 +1,5 @@
+import NoticeHeader from "../../../commons/headers/notice/notice.header.index";
+
+export default function NoticeDetailHeader(): JSX.Element {
+  return <NoticeHeader />;
+}
