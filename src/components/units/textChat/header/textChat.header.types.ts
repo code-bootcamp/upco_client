ec7a131269e1b9cc3933af8e8ae7d9afbcf70c32
kkasol <kkasol92@gmail.com>
2023-04-03 16:09:59 +0900

    [2023.4.3] Docs api 주소 변경

--- src/components/units/textChat/header/textChat.header.types.ts
+++ src/components/units/textChat/header/textChat.header.types.ts
@@ -1,8 +1,7 @@
 export interface TextChatHeaderProps {
   isVideo: boolean;
   messages: Array<{ contents: string; isSent: boolean }>;
-  userId: string;
   messageLog: any;
   myId: string;
-  roomId: boolean;
+  roomId: string;
 }
