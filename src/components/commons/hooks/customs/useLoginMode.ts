3df5c824d0154c68b10de47bd3e86277d7241d3f
“eggmun98” <eggmun98@gmail.com>
2023-04-04 22:52:26 +0900

    [2023.04.04] Feat 로그인, 회원가입 후 모달창

--- src/components/commons/hooks/customs/useLoginMode.ts
+++ src/components/commons/hooks/customs/useLoginMode.ts
@@ -1,9 +1,14 @@
 import { NextRouter, useRouter } from "next/router";
 import { useRecoilState } from "recoil";
 import { FormValues } from "../../items/modal/login/login.types";
-import { accessTokenState } from "../../stores";
+import {
+  accessTokenState,
+  isFindState,
+  isJoinState,
+  isLoginState,
+  showModalState,
+} from "../../stores";
 import { useMutationLogin } from "../mutation/useMutationLogin";
-import { Modal } from "antd";
 
 export const useLoginMode = (): {
   onSubmit: (data: FormValues) => Promise<void>;
@@ -11,6 +16,10 @@ export const useLoginMode = (): {
   const [_, setAccessToken] = useRecoilState(accessTokenState);
   const [login] = useMutationLogin();
   const router: NextRouter = useRouter();
+  const [showModal, setShowModal] = useRecoilState(showModalState);
+  const [isOpen, setIsOpen] = useRecoilState(isLoginState);
+  const [isJoin, setIsJoin] = useRecoilState(isJoinState);
+  const [isFind, setIsFind] = useRecoilState(isFindState);
 
   const onSubmit = async (data: FormValues): Promise<void> => {
     const { email, password } = data;
@@ -22,16 +31,13 @@ export const useLoginMode = (): {
       alert("로그인에 실패하였습니다. 다시 시도해주세요!");
       return;
     }
-
     setAccessToken(accessToken);
 
-    // localStorage.setItem("accessToken", accessToken); // 임시 사용 보안상 좋지 않으므로! 나중에 지울 예정!
-
-    // Modal("로그인 되었습니다");
-    alert("로그인되었습니다.");
+    setShowModal(true);
+    setIsOpen(false);
 
     if ("/".includes(router.asPath)) {
-      router.push("/main");
+      // router.push("/main");
     }
   };
 
--- src/components/units/home/index.tsx
+++ src/components/units/home/index.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
 import FindUI from "../../commons/items/modal/find/find.index";
 import JoinUI from "../../commons/items/modal/join/join.index";
 import LoginUI from "../../commons/items/modal/login/login.index";
 import HomeBody from "./body/home.body.index";
 import HomeHeader from "./header/home.header.index";
+import { useRecoilState } from "recoil";
+import { isFindState, isJoinState, isLoginState, showModalState } from "../../commons/stores";
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useRouter } from "next/router";
 
 export default function HomeUIPage(): JSX.Element {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isJoin, setIsJoin] = useState(false);
-  const [isFind, setIsFind] = useState(false);
+  const [isOpen, setIsOpen] = useRecoilState(isLoginState);
+  const [isJoin, setIsJoin] = useRecoilState(isJoinState);
+  const [isFind, setIsFind] = useRecoilState(isFindState);
+  const [showModal, setShowModal] = useRecoilState(showModalState);
+  const router = useRouter();
 
   const onClickOpen = (open: string) => (): void => {
     if (open === "login") {
@@ -16,6 +22,10 @@ export default function HomeUIPage(): JSX.Element {
     } else if (open === "join") {
       setIsJoin((prev) => !prev);
     }
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+    router.push("/main");
   };
 
   return (
@@ -25,6 +35,27 @@ export default function HomeUIPage(): JSX.Element {
       {isFind && <FindUI setIsOpen={setIsOpen} setIsFind={setIsFind} />}
       <HomeHeader onClickOpen={onClickOpen} />
       <HomeBody onClickOpen={onClickOpen} />
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        style={{
+          display: "flex",
+          justifyContent: "row",
+          flexDirection: "center",
+        }}
+      >
+        <Modal.Body style={{ textAlign: "center" }}>
+          {!isOpen ? "로그인 되었습니다." : "회원가입 되었습니다."}
+        </Modal.Body>
+        <Modal.Footer style={{ border: "none" }}>
+          <Button
+            style={{ backgroundColor: "#6658CA", borderColor: "#6658CA" }}
+            onClick={handleCloseModal}
+          >
+            닫기
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
