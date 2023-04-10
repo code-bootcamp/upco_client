0b26e4338f561ff986c68768ee0b09a5a9868055
chlgusrb97 <ikolp34@gmail.com>
2023-04-10 16:20:18 +0900

    Docs 코드수정

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,8 +1,5 @@
-import { useAuth } from "../../src/components/commons/hooks/customs/useAuth";
 import ProfileUI from "../../src/components/units/profile";
 
 export default function ProfilePage(): JSX.Element {
-  useAuth();
-
   return <ProfileUI />;
 }
